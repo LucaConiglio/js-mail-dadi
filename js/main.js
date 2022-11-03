5d454af42077ed9aeeb27e30212c483b93b5819b
LucaConiglio <lucaconiglio.92@gmail.com>
2022-11-03 16:20:05 +0100

    terminato

--- js/main.js
+++ js/main.js
@@ -8,16 +8,16 @@ console.log(emails);
 let inserisciMail = prompt("inserisci una mail valida");
 
 console.log(inserisciMail);
-let elTrovato = false
+let elTrovato = "thx for tutorial"
 
 for (let i = 0; i < emails.length; i++){
     if (inserisciMail === emails[i]){
-        elTrovato = true
+        elTrovato = "EZ"
         
     }
     
 }
-if (elTrovato === true){
+if (elTrovato !== "thx for tutorial"){
     alert(" ho trovato la tua mail puoi entrare")
 } else{
 alert("non ho trovato la tua mail, perfavore ricarica la pagina e reinserisci la tua mail");
